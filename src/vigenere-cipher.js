1e79577ef8a4e1cb0a4f6b7c325b4a6911806ab2
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-04-17 20:56:38 +0600

    vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -2,30 +2,38 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
- * direct and reverse ciphering machines according to task description
- * 
- * @example
- * 
- * const directMachine = new VigenereCipheringMachine();
- * 
- * const reverseMachine = new VigenereCipheringMachine(false);
- * 
- * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
- * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
- * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
- * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ * direct and reverse ciphering machines according to task description * 
+ * @example * 
+ * const directMachine = new VigenereCipheringMachine(); 
+ * const reverseMachine = new VigenereCipheringMachine(false); 
+ * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!' 
+ * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!' 
+ * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA' 
+ * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA' 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
+  constructor(type) {
+    this.type = type === false ? 'reverseMachine' : 'directMachine';
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    };
+    
+    return message = this.type === 'directMachine' ? message.toUpperCase() : message.toUpperCase().split("").reverse().join(""); 
+    
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+
+  decrypt(encryptedMessage , key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    };
+    return encryptedMessage = this.type === 'directMachine' ? encryptedMessage.toUpperCase() : encryptedMessage.toUpperCase().split("").reverse().join(""); 
+    
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
